24f0d2c37ba8de328d9c2170d5781ad2c519cca1
a.viazovskyi <sentynel2014@gmail.com>
2026-01-28 22:09:59 +0100

    Added mobile menu

--- js/modal.js
+++ js/modal.js
@@ -5,6 +5,8 @@
     modal: document.querySelector('[data-modal]')
   }
 
+  if (!refs.openModalBtn || !refs.closeModalBtn || !refs.modal) return
+
   refs.openModalBtn.addEventListener('click', toggleModal)
   refs.closeModalBtn.addEventListener('click', toggleModal)
 
@@ -12,3 +14,25 @@
     refs.modal.classList.toggle('is-open')
   }
 })()
+;(() => {
+  const refs = {
+    openBtn: document.querySelector('.burger-btn'),
+    closeBtn: document.querySelector('.mobile-menu .modal-close-btn'),
+    menu: document.querySelector('.mobile-menu')
+  }
+
+  if (!refs.openBtn || !refs.closeBtn || !refs.menu) return
+
+  refs.openBtn.addEventListener('click', toggleMenu)
+  refs.closeBtn.addEventListener('click', toggleMenu)
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-open')
+
+    document.body.style.overflow = refs.menu.classList.contains('is-open')
+      ? 'hidden'
+      : ''
+  }
+})()
+
+closeBtn: document.querySelector('.mobile-menu .modal-close-btn')
